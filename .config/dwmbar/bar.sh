3d6c47d14ecb02180bc418d70b5879b58a17ac7c
praneeth-katuri <praneethkaturi@outlook.com>
2024-09-15 19:11:08 +0530

    added configuration files

---
+++ .config/dwmbar/bar.sh
@@ -0,0 +1,53 @@
+#!/bin/dash
+
+# ^c$var^ = fg color
+# ^b$var^ = bg color
+
+interval=0
+
+# load colors
+. ~/.config/dwmbar/bar_themes/onedark
+
+cpu() {
+  cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
+
+  printf "^c$black^ ^b$green^ CPU"
+  printf "^c$white^ ^b$grey^ $cpu_val"
+}
+
+pkg_updates() {
+  updates=$({ timeout 20 checkupdates 2>/dev/null || true; } | wc -l) # arch
+
+
+  if [ -z "$updates" ]; then
+    printf "  ^c$green^    Fully Updated"
+  else
+    printf "  ^c$green^    $updates"" updates"
+  fi
+}
+
+
+mem() {
+  printf "^c$blue^^b$black^  "
+  printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
+}
+
+wlan() {
+	case "$(cat /sys/class/net/wifi0/operstate 2>/dev/null)" in
+	up) printf "^c$black^ ^b$blue^ 󰤨 ^d^%s" " ^c$blue^Connected" ;;
+	down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;;
+	esac
+}
+
+clock() {
+	printf "^c$black^ ^b$darkblue^ 󱑆 "
+	printf "^c$black^^b$blue^ $(date '+%b %d - %I:%M %p') "
+}
+
+while true; do
+
+  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+  interval=$((interval + 1))
+
+  sleep 1 && xsetroot -name "$updates $(cpu) $(mem) $(wlan) $(clock)"
+done
