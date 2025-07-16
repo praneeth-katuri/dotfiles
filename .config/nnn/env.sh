770b8d0b55767afeea350fc4a23601538ced2ba9
Praneeth katuri <132742639+praneeth-katuri@users.noreply.github.com>
2025-07-17 00:11:55 +0530

    feat: add pipewire custom configuration

---
+++ .config/nnn/env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export NNN_BMS="h:$HOME;p:$HOME/Projects;d:$HOME/Downloads;m:/media/data;u:/run/media/$USER;g:/run/user/$(id -u)/gvfs"
+export NNN_PLUG='p:preview-tui;d:dragdrop;n:nmount;m:mtpmount'
+export NNN_FIFO='/tmp/nnn.fifo'
+export NNN_COLORS="2136"
+export NNN_TRASH=1
+export NNN_OPTS='aeHxQDJ'
